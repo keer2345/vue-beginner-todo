d2c5a3c2ee5d44c3ebb9b399d174cd68509f4fd1
keer2345 <keer2345@gmail.com>
2018-10-21 12:30:45 +0800

    crud

---
+++ app.js
@@ -0,0 +1,51 @@
+const LOCAL_STORAGE_KEY = 'todo-app-vue';
+
+const todoApp = new Vue({
+  el: "#app",
+  data: {
+    title: "Todo App",
+    todos: [
+      { text: "Learn JavaScript ES6+ goodies", isDone: true },
+      { text: "Learn Vue", isDone: false },
+      { text: "Build something awesome", isDone: false }
+    ],
+    editing: ""
+  },
+  methods: {
+    createTodo(event) {
+      const textbox = event.target;
+      this.todos.push({ text: textbox.value.trim(), isDone: false });
+      textbox.value = "";
+    },
+    startEditing(todo) {
+      this.editing = todo;
+    },
+    finishEditing(event) {
+      if (!this.editing) {
+        return;
+      }
+      const textbox = event.target;
+      this.editing.text = textbox.value.trim();
+      this.editing = null;
+      textbox.value = "";
+    },
+    cancelEditing() {
+      this.editing = null;
+    },
+    destroyTodo(todo) {
+      const index = this.todos.indexOf(todo);
+      this.todos.splice(index, 1);
+    },
+    clearCompleted() {
+      this.todos = this.activeTodos;
+    }
+  },
+  computed: {
+    activeTodos() {
+      return this.todos.filter(t => !t.isDone);
+    },
+    completedTodos() {
+      return this.todos.filter(t => t.isDone);
+    }
+  }
+});
